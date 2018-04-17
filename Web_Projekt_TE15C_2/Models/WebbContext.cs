f90e0f3563ba86aaa249f758ad50a801d5d1cad1
Elastisken <viktor.schalin@gmail.com>
2018-04-17 12:52:37 +0200

    Added a context for the Article, Gamesdeveloper, Games.

---
+++ Web_Projekt_TE15C_2/Models/WebbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Web_Projekt_TE15C_2.Models
+{
+    public class WebbContext
+    {
+        public DbSet<Articles> Articles { get; set; }
+        public DbSet<GameDevelopers> GamesDevelopers { get; set; }
+        public DbSet<Games> Games { get; set; }
+    }
+}
