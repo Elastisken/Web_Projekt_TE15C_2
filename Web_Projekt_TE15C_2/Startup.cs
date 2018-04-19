5047777861b91bddac8d93f782ffa5089a973cca
Elastisken <viktor.schalin@gmail.com>
2018-04-19 10:20:48 +0200

    Added Customer and Admin roles to log ins.

--- Web_Projekt_TE15C_2/Startup.cs
+++ Web_Projekt_TE15C_2/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using System.Data.Entity;
+using Web_Projekt_TE15C_2.Models;
 
 [assembly: OwinStartupAttribute(typeof(Web_Projekt_TE15C_2.Startup))]
 namespace Web_Projekt_TE15C_2
@@ -9,6 +13,46 @@ namespace Web_Projekt_TE15C_2
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+             CreateRoles();
+             CreateAdmin();
+        }
+
+        private void CreateRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            if (!roleManager.RoleExists("Customer"))
+               {
+              var role = new IdentityRole();
+              role.Name = "Customer";
+              roleManager.Create(role);
+               }
+            if (!roleManager.RoleExists("Admin"))
+              {
+              var role = new IdentityRole();
+              role.Name = "Admin";
+              roleManager.Create(role);
+              }
+            }
+
+    private void CreateAdmin()
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        var userManager =
+            new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+        if (userManager.FindByEmail("admin@admin.com") == null)
+        {
+            var user = new ApplicationUser();
+            user.Email = "admin@admin.com";
+            user.UserName = "admin@admin.com";
+            string password = "Password1!";
+            userManager.Create(user, password);
+            userManager.SetLockoutEnabled(user.Id, false);
+            userManager.AddToRoleAsync(user.Id, "Admin");
+
         }
     }
+  }
 }
